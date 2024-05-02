f4e4080a48e96f7d528b4236cd106577702667c0
sansui233 <sansuilnm@gmail.com>
2024-05-02 14:18:58 +0800

    update pack.mcmeta

--- scripts/zip.ps1
+++ scripts/zip.ps1
@@ -2,3 +2,5 @@
 $outputZipFile = "彩虹帽子v0.1.zip"
 # 使用 7zip 压缩
 & 7z a -tzip "$outputZipFile" "assets" "data" "pack.mcmeta" "pack.png"
+
+Move-Item -Force "彩虹帽子v0.1.zip" "C:\Users\lingn\Documents\Minecraft\Default\.minecraft\versions\1.20.4\saves\新的世界\datapacks\"
